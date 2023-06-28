4726b5d2a57695a6226f617ef3da18d50ba18ec7
sheetalkumari <sheetal.kumari@bjshomedelivery.com>
2023-06-28 13:45:43 +0530

    1st commit

---
+++ client/src/components/Routes/AdminRoute.js
@@ -0,0 +1,65 @@
+// import { useState, useEffect } from "react";
+// import { useAuth } from "../../context/auth";
+// import { Outlet } from "react-router-dom";
+// import axios from "axios";
+// import Spinner from "../Spinner";
+
+// export default function AdminRoute() {
+//   const [ok, setOk] = useState(false);
+//   const [auth, setAuth] = useAuth();
+
+//   useEffect(() => {
+//     const authCheck = async () => {
+//       const res = await axios.get(
+//         "http://localhost:8081/api/v1/auth/admin-auth"
+//       );
+//       if (res.data.ok) {
+//         setOk(true);
+//       } else {
+//         setOk(false);
+//       }
+//     };
+//     if (auth?.token) authCheck();
+//   }, [auth?.token]);
+
+//   return ok ? <Outlet /> : <Spinner path=" " />;
+// }
+import { useState, useEffect } from "react";
+import { useAuth } from "../../context/auth";
+import { Outlet } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../Spinner";
+
+export default function AdminRoute() {
+  const [ok, setOk] = useState(false);
+  const [auth] = useAuth();
+
+  useEffect(() => {
+    const authCheck = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.REACT_APP_API}/api/v1/auth/admin-auth`,
+          {
+            headers: {
+              Authorization: auth?.token,
+            },
+          }
+        );
+        if (res.data.ok) {
+          setOk(true);
+        } else {
+          setOk(false);
+        }
+      } catch (error) {
+        console.log(error);
+        setOk(false);
+      }
+    };
+    if (auth?.token) {
+      authCheck();
+    } else {
+      setOk(false);
+    }
+  }, [auth?.token]);
+  return ok ? <Outlet /> : <Spinner />;
+}
---
+++ client/src/components/Routes/Private.js
@@ -0,0 +1,66 @@
+// import React, { useState, useEffect } from "react";
+// import { useAuth } from "../../context/auth";
+// import { Outlet } from "react-router-dom";
+// import axios from "axios";
+// import Spinner from "../Spinner";
+
+// export default function PrivateRoute() {
+//   const [ok, setOk] = useState(false);
+//   const [auth] = useAuth();
+
+//   useEffect(() => {
+//     const authCheck = async () => {
+//       const res = await axios.get(
+//         "http://localhost:8081/api/v1/auth/user-auth"
+//       );
+//       if (res.data.ok) {
+//         setOk(true);
+//       } else {
+//         setOk(false);
+//       }
+//     };
+//     if (auth?.token) authCheck();
+//   }, [auth?.token]);
+
+//   return ok ? <Outlet /> : <Spinner />;
+// }
+import React, { useState, useEffect } from "react";
+import { useAuth } from "../../context/auth";
+import { Outlet } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../Spinner";
+
+export default function PrivateRoute() {
+  const [ok, setOk] = useState(false);
+  const [auth] = useAuth();
+
+  useEffect(() => {
+    const authCheck = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.REACT_APP_API}/api/v1/auth/user-auth`,
+          {
+            headers: {
+              Authorization: auth?.token,
+            },
+          }
+        );
+
+        if (res.data.ok) {
+          setOk(true);
+        } else {
+          setOk(false);
+        }
+      } catch (error) {
+        console.log(error);
+        setOk(false);
+      }
+    };
+    if (auth?.token) {
+      authCheck();
+    } else {
+      setOk(false);
+    }
+  }, [auth?.token]);
+  return ok ? <Outlet /> : <Spinner />;
+}
